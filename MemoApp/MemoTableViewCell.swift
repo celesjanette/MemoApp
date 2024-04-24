146b9fe4b1703ed2707b937ee94a8ffc54fade8b
celesjanette <celesaug1@gmail.com>
2024-04-24 19:45:39 -0400

    test

---
+++ MemoApp/MemoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoTableViewCell.swift
+//  MemoApp
+//
+//  Created by Celes Augustus on 4/24/24.
+//
+
+import UIKit
+
+class MemoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
